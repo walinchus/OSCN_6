776bc01a102f6db03827c3049215993eaa8344a1
Lucia Walinchus <Lucia@professionalnewsservices.com>
2017-09-08 15:04:05 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,7 +22,7 @@ years = ['2016','2017']
 CrimeSeverity = ['CF','CM']
 
 def CaseEndingNumbers():
-    for x in range(1470, 4721):
+    for x in range(1688, 4721):
         yield '%d' % x
         
 
